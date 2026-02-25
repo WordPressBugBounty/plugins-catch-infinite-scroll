503b6b7436c6a0e1f15cb527674b9bc214e74b35
Sean Murphy <sean@iamseanmurphy.com>
2026-02-25 10:30:47 +0000

    2.1.1

--- catch-infinite-scroll/README.txt
+++ catch-infinite-scroll/README.txt
@@ -4,7 +4,7 @@ Donate link: https://catchplugins.com/plugins/catch-infinite-scroll-pro/
 Tags: infinite scroll, infinite scrolling, infinite, scroll, load more
 Requires at least: 5.9
 Tested up to: 6.9
-Stable tag: 2.1
+Stable tag: 2.1.1
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
@@ -41,6 +41,9 @@ Not so easy way (via FTP) :
 * Go to **Catch Infinite Scroll** from Dashboard menu
 
 == Changelog ==
+
+= 2.1.1 (Released: February 25, 2026) =
+* Bug Fixed: Fixed JS enqueue path for adding catch themes tab item in Themes add theme section
 
 = 2.1 (Released: February 16, 2026) =
 * Bug Fixed: WordPress.Security.NonceVerification.Recommended
--- catch-infinite-scroll/catch-infinite-scroll.php
+++ catch-infinite-scroll/catch-infinite-scroll.php
@@ -16,7 +16,7 @@
  * Plugin Name:       Catch Infinite Scroll
  * Plugin URI:        https://catchplugins.com/plugins/catch-infinite-scroll
  * Description:       Catch Infinite Scroll is a WordPress plugin that allows you to add the magic of infinite scrolling with several customization options on your website without affecting your wallet.
- * Version:           2.1
+ * Version:           2.1.1
  * Author:            Catch Plugins
  * Author URI:        https://catchplugins.com
  * License:           GPL-2.0+
@@ -33,7 +33,7 @@ if (! defined('WPINC')) {
 
 // Define plugin version
 if (! defined('CATCH_INFINITE_SCROLL_VERSION')) {
-	define('CATCH_INFINITE_SCROLL_VERSION', '2.1');
+	define('CATCH_INFINITE_SCROLL_VERSION', '2.1.1');
 }
 
 /**
